9151c81e5cc4264b63241bc1e9d76f8a6e207def
UiCheonLee <62317686+UiCheonLee@users.noreply.github.com>
2020-03-18 11:33:10 +0900

    Create Helloworld.java

---
+++ Helloworld.java
@@ -0,0 +1,12 @@
+package helloworld;
+
+public class helloworld {
+public static void main(String[] args) {
+	int ival=3;
+	double dval=3.14;
+	System.out.println("Hello");
+	System.out.println("Java!!");
+	System.out.print("Hello");
+	System.out.print("Java!!");
+}
+}
