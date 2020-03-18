48566258f4b387f9c482fb01108c6ba8bb9aa64d
lec <lec@DESKTOP-I26FCQK>
2020-03-18 19:50:18 +0900

    removed the star mark

--- src/MenuManager.java
+++ src/MenuManager.java
@@ -1,6 +1,6 @@
 public class MenuManager {
 public static void main(String[] args) {
-	System.out.println("¡ÚWelcome to Review Management Menu¡Ú");
+	System.out.println("Welcome to Review Management Menu!!");
 	System.out.println("Select one number between 1 to 7");
 	System.out.println("1. Add a subject(to review)");
 	System.out.println("2. Delete a subject(to review)");
